fabd03df9a73216e9af9361af04b0ed081564d8b
alexapvl <alex.robite@gmail.com>
2023-11-28 16:23:43 +0200

    Solution for analysis homework 7

--- Semester 1/Analysis/Homeworks/Homework 07/main.py
+++ Semester 1/Analysis/Homeworks/Homework 07/main.py
@@ -1,13 +1,10 @@
 import numpy as np
 
 x = np.linspace(-5, 5, 1000)
-# f(x) = e^(-x)
+# y = f(x) = e^(-x)
 y = np.exp(-x ** 2)
 
-np.sum(y[1:-1])
+sum = np.sum((x[1:] - x[:-1]) / 2 * (y[1:] + y[:-1]))
 
-    
-
-
-# print(f"My result is: {format(np.sum(f))}")
+print(f"My result is: {sum}")
 print(f"Square root of pi is {np.sqrt(np.pi)}")
