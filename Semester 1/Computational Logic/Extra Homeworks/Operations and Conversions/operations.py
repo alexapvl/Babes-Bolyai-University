598889341ab046193e38f7a52e6c6924bbfe8085
alexapvl <alex.robite@gmail.com>
2023-12-02 22:41:12 +0200

    Added comments and documentation

--- Semester 1/Computational Logic/Extra Homeworks/Operations and Conversions/operations.py
+++ Semester 1/Computational Logic/Extra Homeworks/Operations and Conversions/operations.py
@@ -4,11 +4,17 @@ from utilities import *
 
 def addition_in_base_p(a: str, b: str, p: int) -> str:
     """
+    Performs addition in base p of two numbers a and b.
+    First, the numbers are made of the same length by adding leading zeros to the shorter number.
+    Then, the addition is performed digit by digit, from right to left.
+    If the sum of two digits is greater than or equal to p, then the carry is 1 and the digit is the sum minus the base.
+    Otherwise, the carry is 0 and the digit is the sum.
+    If the carry is 1 after the last digit, then the carry is added to the result.
 
-    @param a:
-    @param b:
-    @param p:
-    @return:
+    @param a: first number
+    @param b: second number
+    @param p: base in which the addition is performed
+    @return: the result of the addition in base p of a and b
     """
     a, b = make_same_length(a, b)
     result = ""
@@ -27,11 +33,17 @@ def addition_in_base_p(a: str, b: str, p: int) -> str:
 
 def subtraction_in_base_p(a: str, b: str, p: int) -> str:
     """
+    Performs subtraction in base p of two numbers a and b.
+    First, the numbers are made of the same length by adding leading zeros to the shorter number.
+    Then, the subtraction is performed digit by digit, from right to left.
+    If the difference of two digits is negative, then the borrow is 1 and the digit is the difference plus the base.
+    Otherwise, the borrow is 0 and the digit is the difference.
+    At the end, the leading zeros are removed.
 
-    @param a:
-    @param b:
-    @param p:
-    @return:
+    @param a: first number
+    @param b: second number
+    @param p: base in which the subtraction is performed
+    @return: the result of the subtraction in base p of a and b
     """
     a, b = make_same_length(a, b)
     if base_p_to_base_10(a, p) < base_p_to_base_10(b, p):
@@ -51,11 +63,20 @@ def subtraction_in_base_p(a: str, b: str, p: int) -> str:
 
 def multiplication_by_one_digit_in_base_p(a: str, b: str, p: int) -> str:
     """
+    Performs multiplication in base p of a number a and a digit b.
+    First, we check if the length of a is smaller than the length of b. If so, there is a chance that a is a digit and b is the number.
+    Then, we check if the second parameter is a digit. If not, we raise an error.
+    After that, we perform the multiplication digit by digit, from right to left.
+    The operations are first performed in base 10, then the result is converted in base p.
+    The multiplication is performed from right to left.
+    The carry is the result of the division of the product of the digit of a and b by p.
+    The digit is the remainder of the division of the product of the digit of a and b by p.
+    At the end, if the carry is different from 0, then it is added to the result and the leading zeros are removed.
 
-    @param a:
-    @param b:
-    @param p:
-    @return:
+    @param a: number to be multiplied
+    @param b: the digit by which the multiplication is performed
+    @param p: base in which the multiplication is performed
+    @return: the result of the multiplication in base p of a and digit b
     """
     if len(a) < len(b):
         # commutativity
@@ -75,11 +96,17 @@ def multiplication_by_one_digit_in_base_p(a: str, b: str, p: int) -> str:
 
 def division_by_one_digit_in_base_p(a: str, b: str, p: int) -> tuple:
     """
+    Performs division in base p of a number a and a digit b.
+    First, we check if the second parameter is a digit. If not, we raise an error.
+    Then, we check if the second parameter is 0. If so, we raise an error because we cannot divide by 0.
+    After that, we perform the algorithm for division like we would on paper.
+    All operations are performed in base 10, then the result is converted in base p.
+    The division is performed from left to right.
 
-    @param a:
-    @param b:
-    @param p:
-    @return:
+    @param a: divident
+    @param b: divisor (digit)
+    @param p: base in which the division is performed
+    @return: a tuple containing the quotient and the remainder of the division in base p of a and digit b
     """
     digits = "0123456789ABCDEF"
     if len(b) != 1:
--- Semester 1/Computational Logic/Extra Homeworks/Operations and Conversions/utilities.py
+++ Semester 1/Computational Logic/Extra Homeworks/Operations and Conversions/utilities.py
@@ -3,10 +3,11 @@ from colorama import Fore, Style
 
 def make_same_length(a: str, b: str) -> tuple:
     """
-    
-    @param a:
-    @param b:
-    @return:
+    Makes two numbers of the same length by adding leading zeros to the shorter number.
+    OBS: We use zfill() to add leading zeros to a string.
+    @param a: first number
+    @param b: second number
+    @return: a tuple containing the two numbers of the same length
     """
     a = a.zfill(max(len(a), len(b)))
     b = b.zfill(max(len(a), len(b)))
@@ -14,10 +15,11 @@ def make_same_length(a: str, b: str) -> tuple:
 
 def verify_number_in_base_p(a: str, p: int):
     """
+    Verifies if the number a is in base p.
 
-    @param a:
-    @param p:
-    @return:
+    @param a: number to be verified
+    @param p: base in which the number is verified
+    @return: True if the number is in base p, False otherwise
     """
     if (p < 2 or p > 10) and p != 16:
         raise ValueError(Fore.RED + "Base must be between 2,3,...,10 or 16!" + Style.RESET_ALL)
@@ -38,9 +40,11 @@ def verify_number_in_base_p(a: str, p: int):
 
 def valid_base_p(p: int) -> bool:
     """
+    Verifies if the base p is valid.
+    For our problem statement, a base is valid if it is between 2 and 10 or if it is 16.
 
-    @param p:
-    @return:
+    @param p: base to be verified
+    @return: True if the base is valid, error raised otherwise
     """
     if (p < 2 or p > 10) and p != 16:
         raise ValueError(Fore.RED + "Base must be between 2,3,...,10 or 16!" + Style.RESET_ALL)
@@ -48,9 +52,11 @@ def valid_base_p(p: int) -> bool:
 
 def valid_base_power_of_2(p: int) -> bool:
     """
+    Verifies if the base p is a power of 2.
+    For our problem statement, a base is valid if it is 2, 4, 8 or 16.
 
-    @param p:
-    @return:
+    @param p: base to be verified
+    @return: True if the base is valid, False otherwise
     """
     if p != 2 and p != 4 and p != 8 and p != 16:
         return False
@@ -58,9 +64,12 @@ def valid_base_power_of_2(p: int) -> bool:
 
 def remove_leading_zeros(a: str) -> str:
     """
+    Removes the leading zeros from a number.
+    By removing the leading zeros, the number still has the same value.
+    If our number is 0, then we return 0. (special case)
 
-    @param a:
-    @return:
+    @param a: number from which the leading zeros are removed
+    @return: the number without the leading zeros
     """
     for i in range(len(a)):
         if a[i] != "0":
@@ -69,28 +78,37 @@ def remove_leading_zeros(a: str) -> str:
 
 def digit_to_char(digit: int) -> str:
     """
+    Converts a digit to a character.
+    Useful when we work with base 16.
 
-    @param digit:
-    @return:
+    @param digit: digit to be converted
+    @return: the character corresponding to the digit
     """
     digits = "0123456789ABCDEF"
+    if digit < 0 or digit > 15:
+        raise ValueError(Fore.RED + f"Digit {digit} is not a valid digit!" + Style.RESET_ALL)
     return digits[digit]
 
 def char_to_digit(a: str) -> int:
     """
+    Converts a character to a digit.
+    Useful when we work with base 16.
 
-    @param a:
-    @return:
+    @param a: character to be converted
+    @return: the digit corresponding to the character
     """
     digits = "0123456789ABCDEF"
+    if a not in digits:
+        raise ValueError(Fore.RED + f"Character {a} is not a valid digit!" + Style.RESET_ALL)
     return digits.index(a)
 
 def base_p_to_base_10(number: str, p: int) -> int:
     """
+    Converts a number from base p to base 10 by taking each digit and multiplying it by p to the power of its position.
 
-    @param number:
-    @param p:
-    @return:
+    @param number: number to be converted
+    @param p: base in which the number is
+    @return: the number in base 10
     """
     result: int = 0
     for i in range(len(number)):
@@ -99,10 +117,13 @@ def base_p_to_base_10(number: str, p: int) -> int:
 
 def base_10_to_base_p(number: int, p: int) -> str:
     """
+    Converts a number from base 10 to base p by dividing the number by p and concatenating the remainder at
+    the beginning of the result.
+    This is similar to taking the remainders in reverse order.
 
-    @param number:
-    @param p:
-    @return:
+    @param number: number to be converted, in base 10
+    @param p: base in which the number is
+    @return: the number in base p
     """
     result = ""
     while number != 0:
@@ -112,10 +133,21 @@ def base_10_to_base_p(number: int, p: int) -> str:
 
 def base_p_to_base_2(a: str, p: int):
     """
+    Converts a number from base p to base 2.
+    Base p must be a power of 2. In our case, p can be 2, 4, 8 or 16.
+    If p is 2, then the number is already in base 2, so we return it.
+    If p is 4, 8 or 16, then we convert each digit to its binary representation and concatenate the results.
+    We use power2 to find the number of binary digits needed to represent a digit in base p. For 4 we need 2 digits,
+    for 8 we need 3 digits and for 16 we need 4 digits. This number of digits is stored in num_digits.
+    At the end, we remove the leading zeros from the result.
 
-    @param a:
-    @param p:
-    @return:
+    OBS: 1) We use the bin() function to convert a digit to its binary representation. and we remove the first 2 characters with
+    [2:] because the result of bin() is a string that starts with "0b" if the number is positive or "1b" if the number is negative.
+         2) We use zfill() to add leading zeros to the binary representation of a digit, so that it has num_digits digits.
+
+    @param a: number to be converted
+    @param p: base in which the number is; must be a power of 2
+    @return: the number in base 2
     """
     result = ""
     power2 = 1
@@ -136,10 +168,23 @@ def base_p_to_base_2(a: str, p: int):
 
 def base_2_to_base_p(a: str, p: int):
     """
+    Converts a number from base 2 to base p.
+    Base p must be a power of 2. In our case, p can be 2, 4, 8 or 16.
+    If p is 2, then the number is already in base 2, so we return it.
+    If p is 4, 8 or 16, then we convert each group of p digits to its base p representation and concatenate the results.
+    We use power2 to find the number of binary digits needed to represent a digit in base p. For 4 we need 2 digits,
+    for 8 we need 3 digits and for 16 we need 4 digits. This number of digits is stored in num_digits.
+    At the end, we remove the leading zeros from the result.
 
-    @param a:
-    @param p:
-    @return:
+    OBS: 1) We use int() with the second parameter 2 to convert a binary number to its decimal representation.
+    The 2 is there to tell the interpreter that the first parameter is in base 2.
+         2) We use zfill() to add leading zeros to the binary representation of a digit, so that it has the number of digits
+    needed to represent a digit in base p. This bassically formats the binary number to have the desired number of digits for
+    converting it into base p.
+
+    @param a: number to be converted
+    @param p: base in which the number is; must be a power of 2
+    @return: the number in base p
     """
     result = ""
     power2 = 1
@@ -158,4 +203,4 @@ def base_2_to_base_p(a: str, p: int):
             result += digit_to_char(int(a[i:i+num_digits], 2))
     except ValueError as ve:
         print(ve)
-    return result
+    return remove_leading_zeros(result)
