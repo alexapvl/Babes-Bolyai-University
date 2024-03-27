3bfe69691f8e7adc16c199b430fbd80b7b240eab
alexapvl <alex.robite@gmail.com>
2024-03-27 17:31:11 +0200

    commit

--- Semester 2/Data Structures and Algorithms/Labs/Lab01/FixedCapBiMapIterator.h
+++ Semester 2/Data Structures and Algorithms/Labs/Lab01/FixedCapBiMapIterator.h
@@ -1,19 +1,18 @@
 #pragma once
 #include "FixedCapBiMap.h"
-class FixedCapBiMapIterator
-{
-	//DO NOT CHANGE THIS PART
-	friend class FixedCapBiMap;
-private:
-	const FixedCapBiMap& map;
-	int currentPosition;
+class FixedCapBiMapIterator {
+  // DO NOT CHANGE THIS PART
+  friend class FixedCapBiMap;
 
-	FixedCapBiMapIterator(const FixedCapBiMap& m);
-public:
-	void first();
-	void next();
-	TElem getCurrent();
-	bool valid() const;
+ private:
+  const FixedCapBiMap& map;
+  int currentPosition;
+
+  FixedCapBiMapIterator(const FixedCapBiMap& m);
+
+ public:
+  void first();
+  void next();
+  TElem getCurrent();
+  bool valid() const;
 };
-
-
