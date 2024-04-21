4caf26b10fc96f6574199baa3297c5bcfb2d1416
alexapvl <alex.robite@gmail.com>
2024-04-21 22:55:14 +0300

    added the bag implementation to be done

---
+++ Semester 2/Data Structures and Algorithms/Labs/Lab04/Bag.cpp
@@ -0,0 +1,54 @@
+#include "Bag.h"
+#include "BagIterator.h"
+#include <exception>
+#include <iostream>
+using namespace std;
+
+
+Bag::Bag() {
+	//TODO - Implementation
+}
+
+
+void Bag::add(TElem elem) {
+	//TODO - Implementation
+}
+
+
+bool Bag::remove(TElem elem) {
+	//TODO - Implementation
+	return false; 
+}
+
+
+bool Bag::search(TElem elem) const {
+	//TODO - Implementation
+	return false; 
+}
+
+int Bag::nrOccurrences(TElem elem) const {
+	//TODO - Implementation
+	return 0; 
+}
+
+
+int Bag::size() const {
+	//TODO - Implementation
+	return 0;
+}
+
+
+bool Bag::isEmpty() const {
+	//TODO - Implementation
+	return 0;
+}
+
+BagIterator Bag::iterator() const {
+	return BagIterator(*this);
+}
+
+
+Bag::~Bag() {
+	//TODO - Implementation
+}
+
