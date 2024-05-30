48bed969d137b45803f1168a8cce96d345b9d9b5
alexapvl <alex.robite@gmail.com>
2024-05-30 10:24:30 +0300

    commit

--- Semester 2/Data Structures and Algorithms/Labs/Lab06/SortedBag.h
+++ Semester 2/Data Structures and Algorithms/Labs/Lab06/SortedBag.h
@@ -1,46 +1,50 @@
 #pragma once
-//DO NOT INCLUDE SORTEDBAGITERATOR
+/*
+ADT SortedBag - using BST with linked representation with dynamic allocation.
+In the BST(unique element, frequency) pairs are stored
+*/
+// DO NOT INCLUDE SORTEDBAGITERATOR
 
-//DO NOT CHANGE THIS PART
+// DO NOT CHANGE THIS PART
 typedef int TComp;
 typedef TComp TElem;
-typedef bool(*Relation)(TComp, TComp);
+typedef bool (*Relation)(TComp, TComp);
 #define NULL_TCOMP -11111;
 
 class SortedBagIterator;
 
 class SortedBag {
-	friend class SortedBagIterator;
+  friend class SortedBagIterator;
 
 private:
-	//TODO - Representation
+  // TODO - Representation
 
 public:
-	//constructor
-	SortedBag(Relation r);
+  // constructor
+  SortedBag(Relation r);
 
-	//adds an element to the sorted bag
-	void add(TComp e);
+  // adds an element to the sorted bag
+  void add(TComp e);
 
-	//removes one occurence of an element from a sorted bag
-	//returns true if an eleent was removed, false otherwise (if e was not part of the sorted bag)
-	bool remove(TComp e);
+  // removes one occurence of an element from a sorted bag
+  // returns true if an eleent was removed, false otherwise (if e was not part of the sorted bag)
+  bool remove(TComp e);
 
-	//checks if an element appearch is the sorted bag
-	bool search(TComp e) const;
+  // checks if an element appearch is the sorted bag
+  bool search(TComp e) const;
 
-	//returns the number of occurrences for an element in the sorted bag
-	int nrOccurrences(TComp e) const;
+  // returns the number of occurrences for an element in the sorted bag
+  int nrOccurrences(TComp e) const;
 
-	//returns the number of elements from the sorted bag
-	int size() const;
+  // returns the number of elements from the sorted bag
+  int size() const;
 
-	//returns an iterator for this sorted bag
-	SortedBagIterator iterator() const;
+  // returns an iterator for this sorted bag
+  SortedBagIterator iterator() const;
 
-	//checks if the sorted bag is empty
-	bool isEmpty() const;
+  // checks if the sorted bag is empty
+  bool isEmpty() const;
 
-	//destructor
-	~SortedBag();
+  // destructor
+  ~SortedBag();
 };
