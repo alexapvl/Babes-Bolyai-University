dd43b576c236291cf1672a42545b50d4fa451365
alexapvl <alex.robite@gmail.com>
2024-10-17 23:15:45 +0300

    corrected homework for lab

--- Semester3/Logical_and_Functional_Programming/Lab/Lab1Week1/hw/hw.pl
+++ Semester3/Logical_and_Functional_Programming/Lab/Lab1Week1/hw/hw.pl
@@ -1,12 +1,22 @@
+isMember(X, [X]):-!.
+
+isMember(X, [H|_]) :-
+    X is H,
+    !.
+
+isMember(X, [H|T]) :-
+    \+ X is H,
+    isMember(X, T).
+
 % Base case: The union of an empty set and any set is that set.
 union([], L, L).
 
 % If the head of the first list is a member of the second, skip it and recurse.
 union([H|T], L, R) :- 
-    member(H, L), 
+    isMember(H, L), 
     union(T, L, R).
 
 % If the head of the first list is not a member of the second, include it in the result.
 union([H|T], L, [H|R]) :- 
-    \+ member(H, L), 
+    \+ isMember(H, L), % use \+ for negation
     union(T, L, R).
