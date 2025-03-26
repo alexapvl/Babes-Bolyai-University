a1ea0e3f5f8d3b75a3eab1da5d54ae0ee5433bdd
alexapvl <alex.robite@gmail.com>
2025-03-26 18:46:32 +0200

    lab 1 dbms

--- Semester4/DBMS/Lab/LabActivity/Setup_DBMS/Setup_DBMS/Form1.Designer.cs
+++ Semester4/DBMS/Lab/LabActivity/Setup_DBMS/Setup_DBMS/Form1.Designer.cs
@@ -35,6 +35,15 @@
             this.dataGridTeams = new System.Windows.Forms.DataGridView();
             this.loadTeamsButton = new System.Windows.Forms.Button();
             this.dataGridDrivers = new System.Windows.Forms.DataGridView();
+            this.UpdateChildTableButton = new System.Windows.Forms.Button();
+            this.DeleteChildRowButton = new System.Windows.Forms.Button();
+            this.AddRecordInChildTableButton = new System.Windows.Forms.Button();
+            this.driverNameTextBox = new System.Windows.Forms.TextBox();
+            this.driverNationalityTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.driverDoBDateTimePicker = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.f1DataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.f1DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridTeams)).BeginInit();
@@ -65,6 +74,7 @@
             // 
             this.dataGridTeams.AllowUserToAddRows = false;
             this.dataGridTeams.AllowUserToDeleteRows = false;
+            this.dataGridTeams.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridTeams.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridTeams.Location = new System.Drawing.Point(12, 181);
             this.dataGridTeams.Name = "dataGridTeams";
@@ -78,9 +88,9 @@
             // 
             // loadTeamsButton
             // 
-            this.loadTeamsButton.Location = new System.Drawing.Point(247, 14);
+            this.loadTeamsButton.Location = new System.Drawing.Point(250, 12);
             this.loadTeamsButton.Name = "loadTeamsButton";
-            this.loadTeamsButton.Size = new System.Drawing.Size(191, 123);
+            this.loadTeamsButton.Size = new System.Drawing.Size(210, 125);
             this.loadTeamsButton.TabIndex = 2;
             this.loadTeamsButton.Text = "Load Teams";
             this.loadTeamsButton.UseVisualStyleBackColor = true;
@@ -88,19 +98,110 @@
             // 
             // dataGridDrivers
             // 
+            this.dataGridDrivers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridDrivers.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridDrivers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridDrivers.Location = new System.Drawing.Point(1121, 181);
+            this.dataGridDrivers.Location = new System.Drawing.Point(1127, 181);
             this.dataGridDrivers.Name = "dataGridDrivers";
             this.dataGridDrivers.RowHeadersWidth = 62;
             this.dataGridDrivers.RowTemplate.Height = 28;
+            this.dataGridDrivers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridDrivers.Size = new System.Drawing.Size(1016, 1007);
             this.dataGridDrivers.TabIndex = 3;
+            // 
+            // UpdateChildTableButton
+            // 
+            this.UpdateChildTableButton.Location = new System.Drawing.Point(1933, 12);
+            this.UpdateChildTableButton.Name = "UpdateChildTableButton";
+            this.UpdateChildTableButton.Size = new System.Drawing.Size(210, 125);
+            this.UpdateChildTableButton.TabIndex = 4;
+            this.UpdateChildTableButton.Text = "Update Selected Child Row";
+            this.UpdateChildTableButton.UseVisualStyleBackColor = true;
+            this.UpdateChildTableButton.Click += new System.EventHandler(this.UpdateChildTableButton_Click);
+            // 
+            // DeleteChildRowButton
+            // 
+            this.DeleteChildRowButton.Location = new System.Drawing.Point(1673, 12);
+            this.DeleteChildRowButton.Name = "DeleteChildRowButton";
+            this.DeleteChildRowButton.Size = new System.Drawing.Size(210, 125);
+            this.DeleteChildRowButton.TabIndex = 5;
+            this.DeleteChildRowButton.Text = "Delete Selected Child Row";
+            this.DeleteChildRowButton.UseVisualStyleBackColor = true;
+            this.DeleteChildRowButton.Click += new System.EventHandler(this.DeleteChildRowButton_Click);
+            // 
+            // AddRecordInChildTableButton
+            // 
+            this.AddRecordInChildTableButton.Location = new System.Drawing.Point(1416, 12);
+            this.AddRecordInChildTableButton.Name = "AddRecordInChildTableButton";
+            this.AddRecordInChildTableButton.Size = new System.Drawing.Size(210, 125);
+            this.AddRecordInChildTableButton.TabIndex = 6;
+            this.AddRecordInChildTableButton.Text = "Add Record in Child Table";
+            this.AddRecordInChildTableButton.UseVisualStyleBackColor = true;
+            this.AddRecordInChildTableButton.Click += new System.EventHandler(this.AddRecordInChildTableButton_Click);
+            // 
+            // driverNameTextBox
+            // 
+            this.driverNameTextBox.Location = new System.Drawing.Point(1077, 12);
+            this.driverNameTextBox.Name = "driverNameTextBox";
+            this.driverNameTextBox.Size = new System.Drawing.Size(301, 26);
+            this.driverNameTextBox.TabIndex = 7;
+            // 
+            // driverNationalityTextBox
+            // 
+            this.driverNationalityTextBox.Location = new System.Drawing.Point(1077, 61);
+            this.driverNationalityTextBox.Name = "driverNationalityTextBox";
+            this.driverNationalityTextBox.Size = new System.Drawing.Size(301, 26);
+            this.driverNationalityTextBox.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(928, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Driver name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(928, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(131, 26);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Driver nationality:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(928, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(143, 26);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "Driver date of birth:";
+            // 
+            // driverDoBDateTimePicker
+            // 
+            this.driverDoBDateTimePicker.Location = new System.Drawing.Point(1077, 111);
+            this.driverDoBDateTimePicker.MinDate = new System.DateTime(1960, 1, 1, 0, 0, 0, 0);
+            this.driverDoBDateTimePicker.Name = "driverDoBDateTimePicker";
+            this.driverDoBDateTimePicker.Size = new System.Drawing.Size(301, 26);
+            this.driverDoBDateTimePicker.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2166, 1286);
+            this.Controls.Add(this.driverDoBDateTimePicker);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.driverNationalityTextBox);
+            this.Controls.Add(this.driverNameTextBox);
+            this.Controls.Add(this.AddRecordInChildTableButton);
+            this.Controls.Add(this.DeleteChildRowButton);
+            this.Controls.Add(this.UpdateChildTableButton);
             this.Controls.Add(this.dataGridDrivers);
             this.Controls.Add(this.loadTeamsButton);
             this.Controls.Add(this.dataGridTeams);
@@ -112,6 +213,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridTeams)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridDrivers)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -123,6 +225,15 @@
         private System.Windows.Forms.DataGridView dataGridTeams;
         private System.Windows.Forms.Button loadTeamsButton;
         private System.Windows.Forms.DataGridView dataGridDrivers;
+        private System.Windows.Forms.Button UpdateChildTableButton;
+        private System.Windows.Forms.Button DeleteChildRowButton;
+        private System.Windows.Forms.Button AddRecordInChildTableButton;
+        private System.Windows.Forms.TextBox driverNameTextBox;
+        private System.Windows.Forms.TextBox driverNationalityTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.DateTimePicker driverDoBDateTimePicker;
     }
 }
 
--- Semester4/DBMS/Lab/LabActivity/Setup_DBMS/Setup_DBMS/Form1.cs
+++ Semester4/DBMS/Lab/LabActivity/Setup_DBMS/Setup_DBMS/Form1.cs
@@ -8,12 +8,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace Setup_DBMS
 {
     public partial class Form1: Form
     {
         string connectionString = @"Data Source=DESKTOP-55MRTN0;Initial Catalog=f1;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+        SqlDataAdapter teamsAdapter, driversAdapter;
+        DataSet teamsDataSet = new DataSet();
+        DataSet driversDataSet = new DataSet();
+
+        int? selectedTeamId = null;
 
         public Form1()
         {
@@ -24,6 +31,7 @@ namespace Setup_DBMS
         {
             TestConnection();
         }
+
         private void dataGridTeamsSelectionChanged(object sender, EventArgs e)
         {
             using(SqlConnection conn = new SqlConnection(connectionString))
@@ -32,17 +40,35 @@ namespace Setup_DBMS
                 
                 if (dataGridTeams.CurrentRow != null)
                 {
-                    int teamId = Convert.ToInt32(dataGridTeams.CurrentRow.Cells["teamId"].Value);
-                    SqlCommand cmd = new SqlCommand("select * from driver where teamId=@teamId", conn);
-                    cmd.Parameters.AddWithValue("@teamId", teamId);
+                    this.selectedTeamId = Convert.ToInt32(dataGridTeams.CurrentRow.Cells["teamId"].Value);
+                    LoadDriversBySelectedTeamId();
+                }
+            }
+        }
 
-                    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-                    DataSet dataSet = new DataSet();
-                    adapter.Fill(dataSet, "driver");
-                    dataGridDrivers.DataSource = dataSet.Tables["driver"];
+        private void LoadDriversBySelectedTeamId()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                if(this.selectedTeamId == -1 || this.selectedTeamId == null)
+                {
+                    MessageBox.Show("Please select a team first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
                 }
 
-                conn.Close();
+                SqlCommand cmd = new SqlCommand("SELECT driverId, driverName, teamId, nationality, dateOfBirth FROM driver WHERE teamId = @teamId", conn);
+                cmd.Parameters.AddWithValue("@teamId", this.selectedTeamId);
+
+                this.driversAdapter = new SqlDataAdapter(cmd);
+                this.driversDataSet.Clear();
+                this.driversAdapter.Fill(this.driversDataSet, "driver");
+                dataGridDrivers.DataSource = this.driversDataSet.Tables["driver"];
+
+                // Format the dateOfBirth column
+                if (dataGridDrivers.Columns["dateOfBirth"] != null)
+                {
+                    dataGridDrivers.Columns["dateOfBirth"].DefaultCellStyle.Format = "dd/MM/yyyy"; // the date in the database is not formatted as dd/MM/yyyy, so we need to format it
+                }
             }
         }
 
@@ -51,18 +77,11 @@ namespace Setup_DBMS
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("SELECT * FROM team", conn);
 
-                // SqlDataReader reader = cmd.ExecuteReader();
-                // while (reader.Read())
-                // {
-                //     Console.WriteLine($"Driver ID: {reader["driverId"]}, Name: {reader["driverName"]}");
-                // }
-                // reader.Close();
+                this.teamsAdapter = new SqlDataAdapter(cmd);
+                this.teamsDataSet.Clear();
 
-                SqlDataAdapter daChild = new SqlDataAdapter(cmd);
-                DataSet ds = new DataSet();
-
-                daChild.Fill(ds, "team");
-                dataGridTeams.DataSource = ds.Tables["team"];
+                this.teamsAdapter.Fill(this.teamsDataSet, "team");
+                dataGridTeams.DataSource = this.teamsDataSet.Tables["team"];
                 conn.Close();
             }
         }
@@ -88,6 +107,152 @@ namespace Setup_DBMS
             LoadAllTeams();
         }
 
+        private void AddRecordInChildTableButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                if (this.selectedTeamId == -1 || this.selectedTeamId == null)
+                {
+                    MessageBox.Show("Please select a team in order to add a driver first.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
 
+                SqlCommand cmd = new SqlCommand("INSERT INTO driver (driverName, teamId, nationality, dateOfBirth) VALUES (@driverName, @teamId, @nationality, @dateOfBirth)", conn);
+
+                // take params from the textboxes and dateTimePicker
+                string driverName = driverNameTextBox.Text;
+                string driverNationality = driverNationalityTextBox.Text;
+                string driverDateOfBirth = driverDoBDateTimePicker.Value.ToString("MM/dd/yyyy"); // this is how the date is formatted in the database
+
+                if (driverName == "" || driverNationality == "" || driverDateOfBirth == "")
+                {
+                    MessageBox.Show("Please fill all the fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                cmd.Parameters.AddWithValue("@driverName", driverName);
+                cmd.Parameters.AddWithValue("@teamId", this.selectedTeamId);
+                cmd.Parameters.AddWithValue("@nationality", driverNationality);
+                cmd.Parameters.AddWithValue("@dateOfBirth", driverDateOfBirth);
+                conn.Open();
+                try {
+                    cmd.ExecuteNonQuery();
+                } catch (Exception ex) {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                finally {
+                    conn.Close();
+                }
+                MessageBox.Show("Driver added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            driverNameTextBox.Clear();
+            driverNationalityTextBox.Clear();
+            driverDoBDateTimePicker.Value = DateTime.Now;
+            LoadDriversBySelectedTeamId();
+        }
+
+        private void DeleteChildRowButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                if (dataGridDrivers.SelectedRows.Count > 0)
+                {
+                    // Ask for confirmation before deleting
+                    DialogResult result = MessageBox.Show("Are you sure you want to delete this driver?", 
+                        "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    
+                    if (result == DialogResult.Yes)
+                    {
+                        int driverId = Convert.ToInt32(dataGridDrivers.SelectedRows[0].Cells["driverId"].Value);
+                        SqlCommand cmd = new SqlCommand("DELETE FROM driver WHERE driverId = @driverId", conn);
+                        cmd.Parameters.AddWithValue("@driverId", driverId);
+                        try 
+                        {
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                        } 
+                        catch (Exception ex) 
+                        {
+                            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+                        finally 
+                        {
+                            conn.Close();
+                        }
+                        MessageBox.Show("Driver deleted successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("Please select a driver to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            LoadDriversBySelectedTeamId();
+        }
+
+        private void UpdateChildTableButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                // Get the selected row from the drivers grid
+                if (dataGridDrivers.SelectedRows.Count > 0)
+                {
+                    // Get the ID of the selected driver
+                    int driverId = Convert.ToInt32(dataGridDrivers.SelectedRows[0].Cells["driverId"].Value);
+                    
+                    // Get the updated values from the selected row
+                    string driverName = dataGridDrivers.SelectedRows[0].Cells["driverName"].Value.ToString();
+                    int teamId = Convert.ToInt32(dataGridDrivers.SelectedRows[0].Cells["teamId"].Value);
+                    string nationality = dataGridDrivers.SelectedRows[0].Cells["nationality"].Value.ToString();
+                    string dateOfBirth = dataGridDrivers.SelectedRows[0].Cells["dateOfBirth"].Value.ToString();
+
+                    SqlCommand cmd = new SqlCommand("UPDATE driver SET driverName = @driverName, teamId = @teamId, nationality = @nationality, dateOfBirth = @dateOfBirth WHERE driverId = @driverId", conn);
+                    
+                    cmd.Parameters.AddWithValue("@driverName", driverName);
+                    cmd.Parameters.AddWithValue("@teamId", teamId);
+                    cmd.Parameters.AddWithValue("@nationality", nationality);
+                    cmd.Parameters.AddWithValue("@dateOfBirth", dateOfBirth);
+                    cmd.Parameters.AddWithValue("@driverId", driverId);
+                    
+                    conn.Open();
+                    int rowsAffected = 0;
+
+                    try 
+                    {
+                        rowsAffected = cmd.ExecuteNonQuery();
+                    } 
+                    catch (Exception ex) 
+                    {
+                        if (ex.Message.Contains("FK_referencing_table_team"))
+                        {
+                            MessageBox.Show("You cannot update the team of a driver to a team that does not exist.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    finally 
+                    {
+                        conn.Close();
+                    }
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Driver updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No changes were made.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a driver to update.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            LoadDriversBySelectedTeamId();
+        }
     }
 }
