7649d2499da1e5e58d5918445a6e7583dff1e254
alexapvl <alex.robite@gmail.com>
2025-05-29 22:53:32 +0300

    template for lab test, will make local branches for each exercise I solve so that I do not compromise the current template, will do the same for the exam as well

---
+++ Semester4/DBMS/Lab/LabTest/s6_921/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace LabTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.dgvPosts = new System.Windows.Forms.DataGridView();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPosts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(74, 67);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.RowHeadersWidth = 82;
+            this.dgvUsers.RowTemplate.Height = 33;
+            this.dgvUsers.Size = new System.Drawing.Size(1031, 427);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // dgvPosts
+            // 
+            this.dgvPosts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPosts.Location = new System.Drawing.Point(1306, 67);
+            this.dgvPosts.Name = "dgvPosts";
+            this.dgvPosts.RowHeadersWidth = 82;
+            this.dgvPosts.RowTemplate.Height = 33;
+            this.dgvPosts.Size = new System.Drawing.Size(1282, 427);
+            this.dgvPosts.TabIndex = 1;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(135, 579);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(183, 82);
+            this.ConnectButton.TabIndex = 2;
+            this.ConnectButton.Text = "CONNECT";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(135, 684);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(183, 82);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "SAVE";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2654, 935);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.dgvPosts);
+            this.Controls.Add(this.dgvUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPosts)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.DataGridView dgvPosts;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Semester4/DBMS/Lab/LabTest/s6_921/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace LabTest
+{
+    public partial class Form1: Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=DESKTOP-55MRTN0;Initial Catalog=MiniFacebook;Integrated Security = true");
+        SqlDataAdapter usersAdapter, postsAdapter; // adapters for each of the tables
+        DataSet dataSet;
+        BindingSource usersBS, postsBS; // binding sources for each of the tables
+        SqlCommandBuilder sqlCommandBuilder; // Dark magic stuff (do not delete)
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            // Initialize the DataSet
+            dataSet = new DataSet();
+
+            // Initialize the adapters for each of the tables
+            usersAdapter = new SqlDataAdapter("select * from Users", conn);
+            postsAdapter = new SqlDataAdapter("select * from Posts", conn);
+
+            // Initialize the SqlCommandBuilder
+            sqlCommandBuilder = new SqlCommandBuilder(postsAdapter); // watch this adapter for changes
+
+            // Fill the DataSet with the data from the tables
+            usersAdapter.Fill(dataSet,"Users");
+            postsAdapter.Fill(dataSet, "Posts");
+
+            // Initialize the BindingSources
+            usersBS = new BindingSource();
+            postsBS = new BindingSource();
+
+            // this actually does the relationship binding, creates a foreign key constraint
+            DataRelation DR = new DataRelation("FK_name_users_posts", dataSet.Tables["Users"].Columns["UserId"], dataSet.Tables["Posts"].Columns["UserId"]);
+
+            // Add the DataRelation to the DataSet
+            dataSet.Relations.Add(DR);
+
+            // Set the DataSource and DataMember for the BindingSources
+            usersBS.DataSource = dataSet;
+            usersBS.DataMember = "Users";
+
+            postsBS.DataSource = usersBS;
+            postsBS.DataMember = "FK_name_users_posts";
+
+            // Set the DataSource and DataMember for the DataGridView
+            dgvUsers.DataSource = usersBS;
+            dgvPosts.DataSource= postsBS;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Update the child table
+                postsAdapter.Update(dataSet, "Posts"); // the sql command builder will automatically generate the sql command (see line 36)
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
